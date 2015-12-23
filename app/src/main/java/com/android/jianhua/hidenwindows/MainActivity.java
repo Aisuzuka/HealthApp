a30046b01ea31a959d10042598a952711a9cdb42
Aisuzuka <t102360053@ntut.org.tw>
2015-12-23 11:21:12 +0800

    基本畫面建置

--- app/src/main/java/com/android/jianhua/hidenwindows/MainActivity.java
+++ app/src/main/java/com/android/jianhua/hidenwindows/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.jianhua.hidenwindows;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
@@ -11,39 +12,87 @@ import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView imageView, imageView1, imageView2;
     Button btn, btn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.button);
-        btn1 = (Button) findViewById(R.id.button1);
-        btn.setOnClickListener(this);
-        btn1.setOnClickListener(this);
+        View decorView = MainActivity.this.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        HomePage();
     }
 
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
-            case R.id.button:
-                btn.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    private void HomePage() {
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView1 = (ImageView) findViewById(R.id.imageView2);
+        imageView2 = (ImageView) findViewById(R.id.imageView3);
+        imageView.setOnClickListener(this);
+        imageView1.setOnClickListener(this);
+        imageView2.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageView:
+                Toast.makeText(MainActivity.this, "正常模式", Toast.LENGTH_SHORT).show();
+                //hidden windows
+//                btn.setSystemUiVisibility(
+//                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                                | View.SYSTEM_UI_FLAG_IMMERSIVE);
                 break;
-            case R.id.button1:
-                btn.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            case R.id.imageView2:
+                Toast.makeText(MainActivity.this, "減重模式", Toast.LENGTH_SHORT).show();
+                //show windows
+//                btn.setSystemUiVisibility(
+//                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                break;
+            case R.id.imageView3:
+                Toast.makeText(MainActivity.this, "設定", Toast.LENGTH_SHORT).show();
+                SettingProfile();
+                //show windows
+//                btn.setSystemUiVisibility(
+//                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                break;
+            case R.id.button2:
+                HomePage();
+                break;
+            case R.id.button3:
                 break;
         }
+    }
+
+    private void SettingProfile() {
+        setContentView(R.layout.normal_mode);
+        btn = (Button) findViewById(R.id.button2);
+        btn1 = (Button) findViewById(R.id.button3);
+        btn.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        String[] item = {"test0", "test1", "test2", "test3", "test4", "test5", "test6", "tset7", "test8", "test9"};
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(itemsAdapter);
     }
 }
